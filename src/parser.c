e805c58d5ebfb708a42ca527bef370b16079750b
DomiKo <xkowal99@gmail.com>
2019-02-09 21:22:33 +0100

    added parser

---
+++ src/parser.c
@@ -0,0 +1,161 @@
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include "parser.h"
+#include <string.h>
+#include <stdio.h>
+
+Bigint *parse_expression(char *str)
+{
+    if (strlen(str) == 0)
+        return bigint_constructori(0);
+
+    // +
+    int bracket_balance = 0;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '(')
+            bracket_balance++;
+        else if (str[i] == ')')
+            bracket_balance--;
+
+        if (str[i] == '+' && bracket_balance == 0)
+        {
+            int size_l = i;
+            int size_p = strlen(str) - 1 - i;
+            char *l = malloc(size_l + 1);
+            char *p = malloc(size_p + 1);
+            memcpy(l, str, size_l);
+            memcpy(p, str + i + 1, size_p);
+
+            Bigint *bl = parse_expression(l);
+            Bigint *bp = parse_expression(p);
+            free(l);
+            free(p);
+            return bigint_add(bl, bp);
+        }
+    }
+
+    // -
+    bracket_balance = 0;
+    for (int i = strlen(str) - 1; i >= 0; i--)
+    {
+        if (str[i] == '(')
+            bracket_balance++;
+        else if (str[i] == ')')
+            bracket_balance--;
+
+        if (str[i] == '-' && bracket_balance == 0)
+        {
+            int size_l = i;
+            int size_p = strlen(str) - 1 - i;
+            char *l = malloc(size_l + 1);
+            char *p = malloc(size_p + 1);
+            memcpy(l, str, size_l);
+            memcpy(p, str + i + 1, size_p);
+
+            Bigint *bl = parse_expression(l);
+            Bigint *bp = parse_expression(p);
+            free(l);
+            free(p);
+            return bigint_sub(bl, bp);
+        }
+    }
+
+    // * and /
+    bracket_balance = 0;
+    for (int i = strlen(str) - 1; i >= 0; i--)
+    {
+        if (str[i] == '(')
+            bracket_balance++;
+        else if (str[i] == ')')
+            bracket_balance--;
+
+        if (str[i] == '*' && bracket_balance == 0)
+        {
+            int size_l = i;
+            int size_p = strlen(str) - 1 - i;
+            char *l = malloc(size_l + 1);
+            char *p = malloc(size_p + 1);
+            memcpy(l, str, size_l);
+            memcpy(p, str + i + 1, size_p);
+
+            Bigint *bl = parse_expression(l);
+            Bigint *bp = parse_expression(p);
+            free(l);
+            free(p);
+            return bigint_mult(bl, bp);
+        }
+        else if (str[i] == '/' && bracket_balance == 0)
+        {
+            int size_l = i;
+            int size_p = strlen(str) - 1 - i;
+            char *l = malloc(size_l + 1);
+            char *p = malloc(size_p + 1);
+            memcpy(l, str, size_l);
+            memcpy(p, str + i + 1, size_p);
+
+            Bigint *bl = parse_expression(l);
+            Bigint *bp = parse_expression(p);
+            free(l);
+            free(p);
+            return bigint_div(bl, bp);
+        }
+    }
+
+    // ( )
+    int pl = -1, pr = -1;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == '(')
+        {
+            pl = i;
+            break;
+        }
+    }
+
+    for (int i = strlen(str) - 1; i >= 0; i--)
+    {
+        if (str[i] == ')')
+        {
+            pr = i;
+            break;
+        }
+    }
+
+    if (pl != -1 && pr != -1 && pl < pr)
+    {
+        char *s = malloc(pr - pl - 1 + 1);
+        memcpy(s, str + pl + 1, pr - pl - 1);
+        return parse_expression(s);
+    }
+
+    // number
+    pl = -1, pr = -1;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            pl = i;
+            break;
+        }
+    }
+
+    for (int i = strlen(str) - 1; i >= 0; i--)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            pr = i;
+            break;
+        }
+    }
+
+    if (pl != -1 && pr != -1 && pl <= pr)
+    {
+        char *s = malloc(pr - pl + 1 + 1);
+        memcpy(s, str + pl, pr - pl + 1);
+        return bigint_constructorstr(s);
+    }
+
+    return bigint_constructori(0);
+}
